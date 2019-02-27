a43d89c396e758658197a6e75d62d0e30cfabe39
Jessica Apolinar <jessicaapolinar@MacBook-Air-de-Jessica.local>
2019-02-27 07:11:25 -0600

    feature-add-hooks-component-button-example-japolinar-27/02/2019

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Counter } from './components/counter'
 import './App.css';
 
 class App extends Component {
@@ -20,6 +21,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <Counter />
       </div>
     );
   }
