3dc30d7940c6bd2cfee1b6c31fffee35d838d1c4
Jessica Apolinar <jessicaapolinar@MacBook-Air-de-Jessica.local>
2019-05-15 07:13:04 -0500

    hooks-use-effect-fetch-api-data-japolinar-15/05/2019

--- src/components/counter/index.js
+++ src/components/counter/index.js
@@ -5,7 +5,33 @@ import '../../utils/indexeddb/exceptions';
 
 export function Counter() {
   // Declare a new state variable, which we'll call "count"
-  const [count, setCount] = useState(0);
+  const [counter, setCounter] = useState(0)
+  const [counter2, setCounter2] = useState(100)
+  const [person, setPerson] = useState(null)
+
+
+  useEffect(() => {
+    // useEffect is called
+    console.log('useEffect is called')
+    return () => {
+      // Called when component unmounts
+    }
+  },[counter])
+
+  const getPerson = async () => {
+    try {
+      const response = await fetch('https://randomuser.me/');
+      const data = await response.json();
+      const [item] = data;
+      console.log('item', item)
+      setPerson(item)
+    } catch (error) {
+      console.log('Error', error)
+    }
+  }
+
+  useEffect(getPerson, [])
+
 
 
   const checkIDB = () => {
@@ -21,6 +47,11 @@ export function Counter() {
       <button onClick={() => checkIDB()}>
         Check Indexed DB
       </button>
+      <div>
+        {
+          person && <b>{person.name}</b>
+        }
+      </div>
 
     </div>
   );
