2e4085f8f5073b1765b1352b3fd23fe012b6ebdf
Jessica Apolinar <jessicaapolinar@MacBook-Air-de-Jessica.local>
2019-10-13 21:47:20 -0500

    validate indexed db 2

--- src/utils/indexeddb/exceptions.js
+++ src/utils/indexeddb/exceptions.js
@@ -2,9 +2,11 @@ import { openDataBase } from './indexeddb';
 
 const request = openDataBase('Exceptions', 1);
 
-request.onsuccess = () => {
-  // alert('success')
-};
-request.onerror= () => {
-  // alert('error')
-};
+if (request !== null && request !== undefined) {
+  request.onsuccess = () => {
+    // alert('success')
+  };
+  request.onerror= () => {
+    // alert('error')
+  };
+}
